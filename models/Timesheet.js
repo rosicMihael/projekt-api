762902d96c68d12cc30afadf639ea15c331ca717
Mihael <mihael@exapmle.com>
2025-09-01 22:05:29 +0200

    prevedeno na hr

--- models/Timesheet.js
+++ models/Timesheet.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const dailyLogSchema = new mongoose.Schema({
   date: { type: String, required: true },
-  from: { type: String, required: true }, // Started working
-  to: { type: String, required: true }, // Finished working
-  hourlyPay: { type: String, required: true }, // Hourly rate
-  earnings: { type: Number, required: true }, // Calculated: hours * hourlyPay
+  from: { type: String, required: true },
+  to: { type: String, required: true },
+  hourlyPay: { type: String, required: true },
+  earnings: { type: Number, required: true },
 });
 
 const TimesheetSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   year: { type: Number, required: true },
-  month: { type: Number, required: true, min: 1, max: 12 }, // 1 = January, 12 = December
-  dailyLogs: [dailyLogSchema], // Array of daily entries
+  month: { type: Number, required: true, min: 1, max: 12 },
+  dailyLogs: [dailyLogSchema],
 });
 
 module.exports = mongoose.model("Timesheet", TimesheetSchema);
