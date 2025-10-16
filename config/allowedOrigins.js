7d087143e3962edc6d212091d0d3bf008961860c
Mihael <mihael@exapmle.com>
2025-10-16 22:53:31 +0200

    promjena ip adrese backend servera

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://projekt-ith4.onrender.com",
   "http://localhost:3000",
-  "https://mihaelrosic.from.hr",
+  "https://render.mihaelrosic.from.hr",
 ];
 
 module.exports = allowedOrigins;
